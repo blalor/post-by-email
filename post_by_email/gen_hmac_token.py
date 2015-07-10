a1feb01a25754c855241b5333e4cd3ff536a45d4
Brian Lalor <blalor@bravo5.org>
2015-07-10 10:51:40 -0400

    Add script to generate hmac token for sender email

---
+++ post_by_email/gen_hmac_token.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+import sys
+import itsdangerous
+import hashlib
+
+
+def main(secret_key, email_addr):
+    sep = "^"
+    signer = itsdangerous.Signer(secret_key, sep=sep, digest_method=hashlib.sha256)
+    
+    print signer.sign(email_addr).split(sep, 2)[1]
+    
+    
+if __name__ == "__main__":
+    main(*sys.argv[1:])
